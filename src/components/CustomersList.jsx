fb94119b0438d416f664c1f36f6fcf7a290bb372
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-02-22 16:30:58 -0800

    list style mod

--- src/components/CustomersList.jsx
+++ src/components/CustomersList.jsx
@@ -11,36 +11,36 @@ import React, { Component } from 'react';
 import '../App.css';
 
 
-const hStyle = {
-    width: "12%",
-    float: "left",
-    padding: "10px",
-    background: "red",
-    overflow: "hidden"
-}
+// const hStyle = {
+//     width: "100px",
+//     // float: "left",
+//     padding: "20px",
+//     background: "red",
+//     overflow: "hidden"
+// }
 
-const bStyle = {
-    width: "12%",
-    float: "left",
-    padding: "10px",
-    background: "orange",
-}
+// const bStyle = {
+//     width: "100px",
+//     // float: "left",
+//     padding: "20px",
+//     background: "orange",
+//     overflow: "hidden"
+// }
 
-const mainStyle = {
-    width: "60%",
-    height: "300px",
-    background: "gray",
-    margin: "200px auto",
-    textAlign: "center"
-}
+// const mainStyle = {
+//     width: "60%",
+//     minHeight: "300px",
+//     background: "gray",
+//     margin: "200px auto",
+//     textAlign: "center"
+// }
 
-const nestedStyle = {
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-around",
-    flexWrap: "wrap",
-    clear: "both"
-}
+// const nestedStyle = {
+//     width: "100%",
+//     display: "flex",
+//     // justifyContent: "space-around",
+//     flexWrap: "no-wrap",
+// }
 
 class CustomerList extends Component {
 
@@ -48,31 +48,32 @@ class CustomerList extends Component {
     getHeader = () => {
         if(this.props.data) {
             let headers = Object.keys(this.props.data[0]);
-             console.log(headers);
+            //  console.log(headers);
             return headers.map((header) => {
-                return <div style={hStyle}>{header}</div>
+                return <div>{header}</div>
             })
         }
     }
 
     render() {
         return (
-            <div className="customers-list-wrapper" style={mainStyle}>
-                <div className="head" style={nestedStyle}>
+            <div className="customers-list-wrapper">
+                <div className="thead">
                     {this.getHeader()}
                 </div>
                 {
                 this.props.data.map((row) => {
                 const { id, name, phone, zip, vin, status, action } = row;  
+                console.log(row);
                     return ( 
-                            <div className="body" style={nestedStyle}>
-                                <div style={bStyle} >{id}</div>
-                                <div style={bStyle}>{name}</div>
-                                <div style={bStyle}>{phone}</div>
-                                <div style={bStyle}>{zip}</div>
-                                <div style={bStyle}>{vin}</div>
-                                <div style={bStyle}>{status}</div>
-                                <div style={bStyle}>{action}</div>
+                            <div className="tbody">
+                                <div>{id}</div>
+                                <div>{name}</div>
+                                <div>{phone}</div>
+                                <div>{zip}</div>
+                                <div>{vin}</div>
+                                <div>{status}</div>
+                                <div>{action}</div>
                             </div>
                     )   
                 })
