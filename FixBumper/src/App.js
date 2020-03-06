9c32409672080fa635b283610df4a3167c4377d2
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-03-06 02:03:13 -0800

    add/edit added

--- FixBumper/src/App.js
+++ FixBumper/src/App.js
@@ -13,16 +13,18 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      customers: []
+      customers: [],
+      newCustomer: {},
+      currentIndex: 0
     }
   }
 // for now backend is python. 
-// Node is giving error
-  componentDidMount(){
+// Node is giving an error
+  componentDidMount() {
     axios.get('/api/customers') 
         .then(res => {
         const apibody = res.data;
-        console.log(apibody)
+        // console.log(apibody)
         this.setState({
             customers: apibody,
         });
@@ -32,13 +34,69 @@ class App extends Component {
     })
 }
 
+onStatusChange = (ind) => {
+  const data=this.state.customers;
+  if (data[ind].status==='Active') {
+      data[ind].status='Progress'
+  } else if(data[ind].status==='Progress') {
+      data[ind].status='Inactive'
+  } else {
+      data[ind].status='Active'
+  }
+  this.setState({[this.state.customers]: data})
+}
+// combine this below 2 functions
+handleChange = (e) => {
+  let tmpCustomer = this.state.newCustomer;
+  tmpCustomer[e.target.name] = e.target.value;
+  this.setState({ newCustomer: tmpCustomer })
+}
+handleEditChange = (e, id) => {
+  // console.log(id)
+  let tmpCustomer = this.state.newCustomer;
+  tmpCustomer[e.target.name] = e.target.value;
+  this.setState({ newCustomer: tmpCustomer })
+}
+addNewCustomer = () => {
+  const allCustomers = this.state.customers;
+  const tmpCustomer = this.state.newCustomer;
+  // TODO: figure out better way than adding id here
+  tmpCustomer.id = allCustomers.length + 1
+  if (Object.entries(tmpCustomer).length !== 0) {
+    allCustomers.push(tmpCustomer);
+  }
+  console.group(allCustomers);
+  this.setState({ newCustomer: {} })
+}
+
+editCustomer = (id) => {
+  const allCustomers = this.state.customers;
+  const theCurrentUser = this.state.customers[id-1];
+  const tmpCustomer = this.state.newCustomer;
+  if (Object.entries(tmpCustomer).length !== 0) {
+    for (let key of Object.keys(tmpCustomer)) {
+      theCurrentUser[key] = tmpCustomer[key];
+      allCustomers[id-1] = theCurrentUser;
+      this.setState({customers: allCustomers})
+    }
+  }
+  this.setState({ newCustomer: {} })
+}
+
   render() {
     if(this.state.customers.length > 0 ) {
       return(
         <Router>
             <Switch >
                 <Route path="/" >
-                    <CustomersList data = {this.state.customers}/>
+                    <CustomersList 
+                      data={this.state}
+                      onStatusChange={this.onStatusChange}
+                      handleChange={this.handleChange}
+                      addNewCustomer={this.addNewCustomer}
+                      editCustomer={this.editCustomer}
+                      handleEditChange={this.handleEditChange}
+                    />
                 </Route>
             </Switch>
         </Router>
--- FixBumper/src/components/CustomersList.jsx
+++ FixBumper/src/components/CustomersList.jsx
@@ -30,11 +30,17 @@ class CustomersList extends Component {
 
     getHeader = () => {
         if(this.props.data) {
-            let headers = Object.keys(this.props.data[0]);
-            //  console.log(headers);
+            // let headers = Object.keys(this.props.data.customers[0]);
+            // console.log(headers);
             // slicing out id
-            let headerExceptId = headers.slice(1);
-            return headerExceptId.map((header, ind) => {
+            // let removedID = headers.splice(1, 1);
+            // console.log(headers);
+    // TODO: somehow API returning in different order : TODO
+            // let first = headers.shift();
+            // headers.push(first);
+            // console.log(headers);
+            let headers = ["name", "phone", "zip", "vin", "status", "action"];
+            return headers.map((header, ind) => {
                 return <div key={ind}>{header}</div>
             })
         }
@@ -43,10 +49,9 @@ class CustomersList extends Component {
     render() {
         const { match, location, history } = this.props;
         let background =  location.state && location.state.background;
-        console.log("background from Customers", background);
-        console.log("location From CustomerList: " , location);
+        // console.log("background from Customers", background);
+        // console.log("location From CustomerList: " , location);
         // console.log(history);
-        
         return (
             <div className="customers-page">
                 < Header />
@@ -61,7 +66,10 @@ class CustomersList extends Component {
                             Add New Customer
                         </Link>
                     <Switch>
-                        { background && <Route path="/add" children={<Modal />} /> }  
+                        { background && <Route path="/add" children={<AddCustomerForm data={this.props.data} 
+                                                                            handleChange={this.props.handleChange}
+                                                                            addNewCustomer ={this.props.addNewCustomer} />  } /> 
+                        }  
                     </Switch>                  
                 </div>
                 <div className="customers-list-wrapper">
@@ -69,21 +77,43 @@ class CustomersList extends Component {
                         {this.getHeader()}
                     </div>
                     {
-                        this.props.data.map((row, ind) => {
+                        this.props.data.customers.map((row, ind) => {
                             // destruction
                             const { name, phone, zip, vin, status, action } = row;  
+
+                            let id= this.props.data.customers[ind].id;
+
                             return ( 
                                 <div className="tbody" key={ind}> 
                                     <div>{name}</div>
                                     <div>{phone}</div>
                                     <div>{zip}</div>
                                     <div>{vin}</div>
-                                    <div><input id="status" type="submit" value={status} /></div>
-                                    <div id="action">{action} <span><FontAwesomeIcon id="edit-customer" icon={faPencilAlt}/> </span> <span><FontAwesomeIcon id="delete-customer" icon={faTrash}/> </span></div>
+                                    <div>
+                                        <input 
+                                            id="status" 
+                                            type="submit" 
+                                            value={status} 
+                                            onClick={() => this.props.onStatusChange(ind)}
+                                        />
+                                    </div>
+                                    <div id="action"> {action}
+                                    <Edit id={id}
+                                            background={background} 
+                                            location={location} 
+                                            ind={ind}
+                                            data={this.props.data}
+                                            handleEditChange={this.props.handleEditChange} 
+                                            editCustomer={this.props.editCustomer} />
+                                        <span>
+                                            <FontAwesomeIcon id="delete-customer" icon={faTrash}/> 
+                                        </span>
+                                    </div>  
                                 </div>
                             )   
                         })
-                    }   
+
+                    } 
                     <Pagination />                 
                 </div>
             </div>
@@ -110,44 +140,115 @@ function Pagination() {
             </div>
 }
 
-function Modal() {
+function AddCustomerForm (props) {
     let history = useHistory();
-    // let { id } = useParams();
-    console.log(history)
+    // console.log(history);
+    // console.log(props);
   
     let back = e => {
       e.stopPropagation();
+
       history.goBack();
     };
-  
+
     return (  
-      <div className="modal-bg" onClick={back}>
+      <div className="modal-bg">
           {/* modal body */}
         <div className="modal">
             <div className="modal-body-wrapper">
                 <div>
                     <button id="modal-header"type="button" >New Customer</button>
                 </div>
+                {/* for now status is required to be Active || Inactive || Progress. And action is disabled. Becuase it should always be empty in our case */}
                 <div>
-                    <input type="input" placeholder="Name" />
-                    <input type="input" placeholder="Phone" />
+                    <input type="input" placeholder="Name" name="name" onChange = {props.handleChange}/>
+                    <input type="input" placeholder="Status" name="status" onChange = {props.handleChange} />
                 </div>
                 <div>
-                    <input type="input" placeholder="Email" />
-                    <input type="input" placeholder="Vin" />
+                    <input type="input" placeholder="Email" name="email" onChange = {props.handleChange}/>
+                    <input type="input" placeholder="Vin" name="vin" onChange = {props.handleChange}/>
                 </div>
                 <div>
-                    <input type="input" placeholder="Phone" />
-                    <input type="input" placeholder="Address" />
+                    <input type="input" placeholder="Phone" name="phone" onChange = {props.handleChange}/>
+                    <input type="input" placeholder="Action" name="action" onChange = {props.handleChange} disabled />
                 </div>
                 <div>
-                    <input type="input" placeholder="Zip Code" />
+                    <input type="input" placeholder="Zip Code" name="zip" onChange = {props.handleChange}/>
                     <span id="checkbox"> slider-icon | Active Customer </span>
                 </div>
                 <div className="add-cancel-row">
-                    <button id="add" type="button" onClick={back}> Add</button>
+                    <button id="add" type="button" onClick={(e) => { props.addNewCustomer(); back(e)}}> Add</button>
                     <button id="cancel" type="button" onClick={back}> Cancel</button>
                 </div>           
+            </div>
+        </div>
+      </div>
+    );
+}
+
+function Edit(props) {
+    let { id, location, background, ind, handleEditChange, editCustomer, data } = props;
+    return (
+        <span>      
+            <Link
+                to={{
+                    pathname: `/edit/${id}`,
+                    // Beware tricky! This link sets the `background` in location state (from CustomerList).
+                    state: { background: location }
+                    }}
+                >
+                    <FontAwesomeIcon id="edit-customer" icon={faPencilAlt}/>
+            </Link>
+            <Switch>
+                { 
+                    background && <Route path="/edit/:id" children={ < EditCustomerForm ind={ind} handleEditChange={handleEditChange} editCustomer={editCustomer} data={data}/> } /> 
+                }  
+            </Switch>           
+        </span>  
+    )
+}
+
+function EditCustomerForm (props) {
+    let history = useHistory();
+    const { id } = useParams();
+    // console.log(id);
+    let { ind, handleEditChange, data, editCustomer } = props;
+
+    let back = e => {
+      e.stopPropagation();
+
+      history.goBack();
+    };
+
+    return (  
+      <div className="modal-bg">
+          {/* modal body */}
+        <div className="modal">
+            <div className="modal-body-wrapper">
+                <div>
+                    <button id="modal-header"type="button" >Edit Customer</button>
+                </div>
+                <div>
+                    {/*  TODO: figure out better way than id-1 */}
+                    <input type="input" placeholder="Name" name="name" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].name}/>
+                    <input type="input" placeholder="Status" name="status" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].status}/>
+                </div>
+                <div>
+                    <input type="input" placeholder="Email" name="email" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].email}/>
+                    <input type="input" placeholder="Vin" name="vin" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].vin}/>
+                </div>
+                <div>
+                    <input type="input" placeholder="Phone" name="phone" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].phone}/>
+                    <input type="input" placeholder="Action" name="action" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].action} />
+                </div>
+                <div>
+                    <input type="input" placeholder="Zip Code" name="zip" onChange = {(e) => handleEditChange(e, id)} defaultValue={data.customers[id-1].zip}/>
+                    <span id="checkbox"> slider-icon | Active Customer </span>
+                </div>
+                <div className="add-cancel-row">
+                    <button id="add" type="button" onClick={(e) => { editCustomer(id); back(e)}} > Submit</button>
+                    <button id="cancel" type="button" onClick={back}> Cancel</button>
+                </div>   
             </div>
         </div>
       </div>
@@ -157,38 +258,3 @@ function Modal() {
 
 export default withRouter(CustomersList);
 
-
-// const AddCustomer = () => {
-//     let location = useLocation();
-//     let background =  location.state && location.state.background;
-//     console.log(location);
-//     console.log(background)
-//         return (
-//             <div>
-//                 <Switch location={background || location}>
-//                     <Route path="/" children={<Home />} />
-//                 </Switch>
-//             {background && <Route path="add" children={<Modal />} />}
-//             </div>
-//         )
-// }
-
-// function Home() {
-//     let location = useLocation();
-//     console.log("location From HOME: ", location)
-  
-//     return (
-//         <div>
-//           <Link
-//             to={{
-//               pathname: "/add",
-//               // This is the trick! This link sets
-//               // the `background` in location state.
-//               state: { background: location }
-//             }}
-//           >
-//               Open Modal
-//           </Link>
-//       </div>
-//     );
-//   }
